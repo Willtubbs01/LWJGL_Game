7da5f82886fc2b696c14861065c1e75778a7d22d
Willtubbs01 <copa.nathan@gmail.com>
2026-09-04 01:16:35 -0500

    Changed Main to use Game

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,10 +1,14 @@
 package org.example;
 
+import org.example.engine.Game;
+import org.example.engine.Window;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
 
-
+        Game game = new Game();
+        game.run();
     }
 }
