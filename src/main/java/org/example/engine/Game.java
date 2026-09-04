c690e89328858f9f6eab61a334c7090975fdc396
Willtubbs01 <copa.nathan@gmail.com>
2026-09-04 01:17:23 -0500

    Created Game class to be the main file of the game

--- src/main/java/org/example/engine/Game.java
+++ src/main/java/org/example/engine/Game.java
@@ -1,4 +1,33 @@
 package org.example.engine;
 
 public class Game {
+
+    private Window window;
+
+    public void run(){
+        init();
+        loop();
+        cleanup();
+    }
+
+    private void init(){
+        window = new Window(
+                1280,
+                720,
+                "Island Survival Game"
+        );
+
+        window.init();
+    }
+
+    private void loop() {
+        while(!window.shouldClose()){
+
+            window.update();
+        }
+    }
+
+    private void cleanup() {
+        window.destroy();
+    }
 }
